86d1990229ef66ab781bd27f612a1aa83f5e3530
Onur Türkölmez <onurtrklmz@gmail.com>
2026-09-17 22:32:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Çentik service worker — sayfayı çevrimdışı açılabilir kılar.
 // index.html'i güncellediğinde SURUM değerini artır.
-const SURUM = "centik-v11-jump-boxes";
+const SURUM = "centik-v12-performance";
 const KABUK = [
   "./",
   "./index.html",
