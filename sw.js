79c9e37f0aa22212021b7cc7df739a62d0adb580
Onur Türkölmez <onurtrklmz@gmail.com>
2026-09-18 13:08:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // Çentik service worker — sayfayı çevrimdışı açılabilir kılar.
 // v25: açılır takvim çekmecesi, dokunmatik ay geçişleri ve takvim görev/biten sayacı; v23 taşıma kararlılığı korunur.
-const SURUM = "centik-v32-calendar-drawer";
+const SURUM = "centik-v33-swipe-drawer";
 const KABUK = [
   "./",
   "./index.html",
